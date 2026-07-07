6625bc21df18f21370efd42b5df1aa1f0e688c7c
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-07-07 15:57:19 +0200

    feat: add action to cta

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import styles from "./Hero.module.css"
 import { slideIn } from "../../animations"
 import { motion } from "framer-motion"
+import { Link } from "react-router-dom"
 
 export default function Hero() {
     return (
@@ -22,8 +23,13 @@ export default function Hero() {
                     transition={{ duration: 0.85, delay: 0.15, ease: "easeOut" }}
                     className={styles.cta}
                 >
-                    <button className={styles.ctaPrimaryBtn}>Rezervisi Odmah</button>
-                    <button className={styles.ctaSecondaryBtn}>Pogledaj Meni</button>
+                    <a href="tel:+381601234567">
+                        <button className={styles.ctaPrimaryBtn}>Rezervisi Odmah</button>
+                    </a>      
+                    <Link to="/menu">
+                        <button className={styles.ctaSecondaryBtn}>Pogledaj Meni</button>
+                    </Link>
+                    
                 </motion.div>
             </div>
         </div>
